bd056358c2407667a6624ee9c104c60a28a20efb
root <root@localhost.localdomain>
2018-04-03 18:38:39 +0800

    update CCache

--- src/CarlerCache/CCache.php
+++ src/CarlerCache/CCache.php
@@ -3,45 +3,43 @@ namespace CarlerCache;
 
 class CCache
 {
-    static $instance;
+    private $instance;
 
     public function __construct($params = [], $cache_type = 'file')
     {
-        if (empty(self::$instance)) {
-            switch ($cache_type) {
-                case 'redis':
-                    if (!extension_loaded('redis')) {
-                        print_r("no extension of redis");
-                        return FALSE;
-                    }
-                    self::$instance = new CRedisCache($params);
-                    break;
-                case 'file':
-                    self::$instance = new  CFileCache($params);
-                    break;
-                case 'ssdb':
-                    self::$instance = new  CSsdbCache($params);
-                    break;
-                default:
+        switch ($cache_type) {
+            case 'redis':
+                if (!extension_loaded('redis')) {
+                    print_r("no extension of redis");
                     return FALSE;
-            }
+                }
+                $this->instance = new CRedisCache($params);
+                break;
+            case 'file':
+                $this->instance = new  CFileCache($params);
+                break;
+            case 'ssdb':
+                $this->instance = new  CSsdbCache($params);
+                break;
+            default:
+                return FALSE;
         }
-        return self::$instance;
+        return $this->instance;
     }
 
     public function get($key)
     {
-        return self::$instance->get($key);
+        return $this->instance->get($key);
     }
 
     public function set($key, $value, $ttl = 360, $mode = 'wb')
     {
-        return self::$instance->set($key, $value, $ttl, $mode);
+        return $this->instance->set($key, $value, $ttl, $mode);
     }
 
     public function delete($key)
     {
-        return self::$instance->delete($key);
+        return $this->instance->delete($key);
     }
 
 }
